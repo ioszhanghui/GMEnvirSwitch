dc2d91773502eaeada9ca3fa65160f3943b19f7b
dorayo <yomingyo@gmail.com>
2019-09-26 10:41:29 +0800

    Initial commit

---
+++ Example/GMEnvirSwitch/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  GMEnvirSwitch
+//
+//  Created by ioszhanghui@163.com on 09/26/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GMEnvirSwitch/GMViewController.h
@@ -0,0 +1,13 @@
+//
+//  GMViewController.h
+//  GMEnvirSwitch
+//
+//  Created by ioszhanghui@163.com on 09/26/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GMViewController : UIViewController
+
+@end
